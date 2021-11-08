da0a3e85d8f442948743c8518bf4657af1c9d6bc
Mark <mark@example.dev>
2021-11-08 15:03:59 -0800

    fix left arrow

--- carousel/src/Carousel.test.js
+++ carousel/src/Carousel.test.js
@@ -13,18 +13,26 @@ it("matches snapshot", function() {
   expect(r.asFragment()).toMatchSnapshot();
 });
 
-it("works when you click on the right arrow", function() {
-  const { queryByTestId, queryByAltText } = render(<Carousel />);
+it("correctly navigates with left and right arrows", function() {
+  const r = render(<Carousel />);
 
   // expect the first image to show, but not the second
-  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
-  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+  expect(r.getByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(r.queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
 
   // move forward in the carousel
-  const rightArrow = queryByTestId("right-arrow");
+  const rightArrow = r.getByTestId("right-arrow");
   fireEvent.click(rightArrow);
 
   // expect the second image to show, but not the first
-  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
-  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+  expect(r.queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(r.getByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+
+  // move backward in the carousel
+  const leftArrow = r.getByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(r.getByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(r.queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
 });
